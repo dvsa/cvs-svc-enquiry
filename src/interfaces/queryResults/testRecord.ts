98e74b2532e39047bb3aade6f7383edcbfbd561a
Robert Waring <Robert.Waring@bjss.com>
2021-03-26 14:45:21 +0000

    fix(cvsb-19222): remove unused import

--- src/interfaces/queryResults/testRecord.ts
+++ src/interfaces/queryResults/testRecord.ts
@@ -1,5 +1,4 @@
 import { RowDataPacket } from 'mysql2';
-import Defect from './defect';
 import FuelEmission from './fuelEmission';
 import TestDefect from './testDefect';
 import Tester from './tester';
