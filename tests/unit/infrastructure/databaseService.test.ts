0167626e8f44ef3161b9ab73f9d51ca6a7bd2f8c
Robert Waring <Robert.Waring@bjss.com>
2021-04-07 12:25:07 +0100

    test(cvsb-19222): added tests for non-existant database connection

--- tests/unit/infrastructure/databaseService.test.ts
+++ tests/unit/infrastructure/databaseService.test.ts
@@ -12,6 +12,32 @@ describe('Database Service', () => {
     dbClusterIdentifier: 'cluster',
   };
 
+  it('should throw an error when the connection fails', async () => {
+    const mockSecretsManager = {
+      getSecret: jest.fn().mockResolvedValueOnce(JSON.stringify(connectionDetails)).mockResolvedValue('dbName'),
+    };
+    const mockMysql = ({
+      createConnection: jest.fn().mockRejectedValue(new Error('this is an error')),
+    } as unknown) as typeof mysqlp;
+
+    const dbService = new DatabaseService(mockSecretsManager, mockMysql);
+
+    await expect(dbService.get('sdfsdf', [''])).rejects.toThrow(Error);
+  });
+
+  it('should throw an error when the query fails', async () => {
+    const mockSecretsManager = {
+      getSecret: jest.fn().mockResolvedValueOnce(JSON.stringify(connectionDetails)).mockResolvedValue('dbName'),
+    };
+    const mockMysql = ({
+      createConnection: jest.fn().mockResolvedValue({ execute: jest.fn().mockRejectedValue(new Error()) }),
+    } as unknown) as typeof mysqlp;
+
+    const dbService = new DatabaseService(mockSecretsManager, mockMysql);
+
+    await expect(dbService.get('sdfsdf', [''])).rejects.toThrow(Error);
+  });
+
   it('should get the connection secret from the secrets manager', async () => {
     const mockSecretsManager = {
       getSecret: jest.fn().mockResolvedValueOnce(JSON.stringify(connectionDetails)).mockResolvedValue('dbName'),
