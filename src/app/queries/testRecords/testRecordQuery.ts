7a5447b02268e558ee66d37c467d0321ac8b7c69
Robert Waring <Robert.Waring@bjss.com>
2021-04-09 14:05:42 +0100

    fix(cvsb-19222): added spaces to fix SQL syntax errors

--- src/app/queries/testRecords/testRecordQuery.ts
+++ src/app/queries/testRecords/testRecordQuery.ts
@@ -1,79 +1,80 @@
-const queryBody = 'SELECT `tr`.`id`, JSON_OBJECT('
-  + "'fuelEmission', JSON_OBJECT("
-  + "'modTypeCode', `fe`.`modTypeCode`, "
-  + "'description', `fe`.`description`, "
-  + "'emissionStandard', `fe`.`emissionStandard`, "
-  + "'fuelType', `fe`.`fuelType`"
-  + '), '
-  + "'test_station', JSON_OBJECT("
-  + "'pNumber', `ts`.`pNumber`,"
-  + "'name', `ts`.`name`,"
-  + "'type', `ts`.`type`"
-  + '),'
-  + "'tester', JSON_OBJECT("
-  + "'staffId', `t`.`staffId`, "
-  + "'name', `t`.`name`, "
-  + "'email_address', `t`.`email_address`"
-  + '),'
-  + "'preparer', JSON_OBJECT("
-  + "'preparerId', `p`.`preparerId`, "
-  + "'name', `p`.`name`"
-  + '), '
-  + "'vehicle_class', JSON_OBJECT("
-  + "'code', `vc`.`code`, "
-  + "'description', `vc`.`description`, "
-  + "'vehicleType', `vc`.`vehicleType`,"
-  + "'vehicleSize', `vc`.`vehicleSize`, "
-  + "'vehicleConfiguration', `vc`.`vehicleConfiguration`,"
-  + "'euVehicleCategory', `vc`.`euVehicleCategory`"
-  + '), '
-  + "'testType', JSON_OBJECT("
-  + "'testTypeClassification', tt.`testTypeClassification`, "
-  + "'testTypeName', tt.`testTypeName`"
-  + '), '
-  + "'testStatus', testStatus, "
-  + "'reasonForCancellation', `reasonForCancellation`, "
-  + "'numberOfSeats', `numberOfSeats`, "
-  + "'odometerReading', `odometerReading`,"
-  + "'odometerReadingUnits', `odometerReadingUnits`,"
-  + "'countryOfRegistration', `countryOfRegistration`,"
-  + "'noOfAxles', `noOfAxles`, "
-  + "'regnDate', `regnDate`, "
-  + "'firstUseDate', `firstUseDate`, "
-  + "'createdAt', `tr`.`createdAt`,"
-  + "'lastUpdatedAt', `lastUpdatedAt`, "
-  + "'testCode', `testCode`, "
-  + "'testNumber', `testNumber`,"
-  + "'certificateNumber', `certificateNumber`, "
-  + "'secondaryCertificateNumber', `secondaryCertificateNumber`, "
-  + "'testExpiryDate', `testExpiryDate`, "
-  + "'testAnniversaryDate', `testAnniversaryDate`,"
-  + "'testTypeStartTimestamp', `testTypeStartTimestamp`, "
-  + "'testTypeEndTimestamp', `testTypeEndTimestamp`, "
-  + "'numberOfSeatbeltsFitted', `numberOfSeatbeltsFitted`, "
-  + "'lastSeatbeltInstallationCheckDate', `lastSeatbeltInstallationCheckDate`,"
-  + "'seatbeltInstallationCheckDate', IF(`tr`.`seatbeltInstallationCheckDate` = 1, cast(TRUE AS json), cast(FALSE AS json)), "
-  + "'testResult', `testResult`,"
-  + "'reasonForAbandoning', `reasonForAbandoning`, "
-  + "'additionalNotesRecorded', `additionalNotesRecorded`,"
-  + "'additionalCommentsForAbandon', `additionalCommentsForAbandon`, "
-  + "'particulateTrapFitted', `particulateTrapFitted`, "
-  + "'particulateTrapSerialNumber', `particulateTrapSerialNumber`,"
-  + "'modificationTypeUsed', modificationTypeUsed, "
-  + "'smokeTestKLimitApplied', `smokeTestKLimitApplied`,"
-  + "'createdById', `i`.`identityId`, "
-  + "'lastUpdatedById', `i2`.`identityId`"
-  + ') result'
-  + 'FROM `test_result` `tr`'
-  + 'JOIN `vehicle` `v` on `tr`.`vehicle_id` = `v`.`id`'
-  + 'JOIN `vehicle_class` `vc` on `tr`.`vehicle_class_id` = `vc`.`id`'
-  + 'JOIN `fuel_emission` `fe` on `tr`.`fuel_emission_id` = `fe`.`id`'
-  + 'JOIN `test_station` `ts` on `tr`.`test_station_id` = `ts`.`id`'
-  + 'JOIN `tester` `t` on `tr`.`tester_id` = `t`.`id`'
-  + 'JOIN `preparer` `p` on `tr`.`preparer_id` = `p`.`id`'
-  + 'JOIN `test_type` `tt` on `tr`.`test_type_id` = `tt`.`id`'
-  + 'LEFT JOIN `identity` i on `tr`.`createdBy_Id` = `i`.`id`'
-  + 'LEFT JOIN `identity` i2 on `tr`.`lastUpdatedBy_Id` = `i2`.`id`';
+const queryBody =
+  'SELECT `tr`.`id`, JSON_OBJECT(' +
+  "'fuelEmission', JSON_OBJECT(" +
+  "'modTypeCode', `fe`.`modTypeCode`, " +
+  "'description', `fe`.`description`, " +
+  "'emissionStandard', `fe`.`emissionStandard`, " +
+  "'fuelType', `fe`.`fuelType`" +
+  '), ' +
+  "'test_station', JSON_OBJECT(" +
+  "'pNumber', `ts`.`pNumber`," +
+  "'name', `ts`.`name`," +
+  "'type', `ts`.`type`" +
+  '),' +
+  "'tester', JSON_OBJECT(" +
+  "'staffId', `t`.`staffId`, " +
+  "'name', `t`.`name`, " +
+  "'email_address', `t`.`email_address`" +
+  '),' +
+  "'preparer', JSON_OBJECT(" +
+  "'preparerId', `p`.`preparerId`, " +
+  "'name', `p`.`name`" +
+  '), ' +
+  "'vehicle_class', JSON_OBJECT(" +
+  "'code', `vc`.`code`, " +
+  "'description', `vc`.`description`, " +
+  "'vehicleType', `vc`.`vehicleType`," +
+  "'vehicleSize', `vc`.`vehicleSize`, " +
+  "'vehicleConfiguration', `vc`.`vehicleConfiguration`," +
+  "'euVehicleCategory', `vc`.`euVehicleCategory`" +
+  '), ' +
+  "'testType', JSON_OBJECT(" +
+  "'testTypeClassification', tt.`testTypeClassification`, " +
+  "'testTypeName', tt.`testTypeName`" +
+  '), ' +
+  "'testStatus', testStatus, " +
+  "'reasonForCancellation', `reasonForCancellation`, " +
+  "'numberOfSeats', `numberOfSeats`, " +
+  "'odometerReading', `odometerReading`," +
+  "'odometerReadingUnits', `odometerReadingUnits`," +
+  "'countryOfRegistration', `countryOfRegistration`," +
+  "'noOfAxles', `noOfAxles`, " +
+  "'regnDate', `regnDate`, " +
+  "'firstUseDate', `firstUseDate`, " +
+  "'createdAt', `tr`.`createdAt`," +
+  "'lastUpdatedAt', `lastUpdatedAt`, " +
+  "'testCode', `testCode`, " +
+  "'testNumber', `testNumber`," +
+  "'certificateNumber', `certificateNumber`, " +
+  "'secondaryCertificateNumber', `secondaryCertificateNumber`, " +
+  "'testExpiryDate', `testExpiryDate`, " +
+  "'testAnniversaryDate', `testAnniversaryDate`," +
+  "'testTypeStartTimestamp', `testTypeStartTimestamp`, " +
+  "'testTypeEndTimestamp', `testTypeEndTimestamp`, " +
+  "'numberOfSeatbeltsFitted', `numberOfSeatbeltsFitted`, " +
+  "'lastSeatbeltInstallationCheckDate', `lastSeatbeltInstallationCheckDate`," +
+  "'seatbeltInstallationCheckDate', IF(`tr`.`seatbeltInstallationCheckDate` = 1, cast(TRUE AS json), cast(FALSE AS json)), " +
+  "'testResult', `testResult`," +
+  "'reasonForAbandoning', `reasonForAbandoning`, " +
+  "'additionalNotesRecorded', `additionalNotesRecorded`," +
+  "'additionalCommentsForAbandon', `additionalCommentsForAbandon`, " +
+  "'particulateTrapFitted', `particulateTrapFitted`, " +
+  "'particulateTrapSerialNumber', `particulateTrapSerialNumber`," +
+  "'modificationTypeUsed', modificationTypeUsed, " +
+  "'smokeTestKLimitApplied', `smokeTestKLimitApplied`," +
+  "'createdById', `i`.`identityId`, " +
+  "'lastUpdatedById', `i2`.`identityId`" +
+  ') result' +
+  ' FROM `test_result` `tr`' +
+  ' JOIN `vehicle` `v` on `tr`.`vehicle_id` = `v`.`id`' +
+  ' JOIN `vehicle_class` `vc` on `tr`.`vehicle_class_id` = `vc`.`id`' +
+  ' JOIN `fuel_emission` `fe` on `tr`.`fuel_emission_id` = `fe`.`id`' +
+  ' JOIN `test_station` `ts` on `tr`.`test_station_id` = `ts`.`id`' +
+  ' JOIN `tester` `t` on `tr`.`tester_id` = `t`.`id`' +
+  ' JOIN `preparer` `p` on `tr`.`preparer_id` = `p`.`id`' +
+  ' JOIN `test_type` `tt` on `tr`.`test_type_id` = `tt`.`id`' +
+  ' LEFT JOIN `identity` i on `tr`.`createdBy_Id` = `i`.`id`' +
+  ' LEFT JOIN `identity` i2 on `tr`.`lastUpdatedBy_Id` = `i2`.`id`';
 
 const TEST_RECORD_BY_VIN = `${queryBody} WHERE \`v\`.\`vin\` = ?`;
 const TEST_RECORD_BY_VRM = `${queryBody} WHERE \`v\`.\`vrm_trm\` = ?`;
