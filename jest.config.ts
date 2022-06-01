b8a210ef24da0cefbf6a98911fbd3f4c961fdda0
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-06-01 15:09:02 +0100

    refactor(cb2-3295): added remove to s3 upload callback

--- jest.config.ts
+++ jest.config.ts
@@ -29,10 +29,10 @@ const config: Config.InitialOptions = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75,
     },
   },
   globals: {
