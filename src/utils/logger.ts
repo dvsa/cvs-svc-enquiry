8aeed61e78927cf3a29629cf716cdbfeb561e64e
tom-bjss <92319736+tom-bjss@users.noreply.github.com>
2022-07-18 15:19:29 +0100

    Update src/utils/logger.ts
    Co-authored-by: Taha Yazici <105922152+tahayazici@users.noreply.github.com>

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,13 +2,11 @@ import { createLogger, format, transports } from 'winston';
 
 const { printf } = format;
 
-const logFormat = printf((info) => {
-  // Checks if log is an error - has stack info
-  if (info.stack) {
-    return `${info.level}: ${info.stack as string}`;
-  }
-  return `${info.level}: ${info.message as string}`;
-});
+// Checks if log is an error - has stack info
+const logFormat = printf(info => info.stack
+  ? `${info.level}: ${info.stack as string}`
+  : `${info.level}: ${info.message as string}`
+);
 
 const loggerConfig = {
   level: process.env.LOG_LEVEL || 'info',
