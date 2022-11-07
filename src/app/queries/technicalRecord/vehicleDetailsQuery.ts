2348a78cf5b6216f0df27e411ccf22131e5d2b19
ian-hodges <ian.hodges@bjss.com>
2022-11-07 12:49:09 +0000

    refactor(cb2-5365): make sql query generation easier to read

--- src/app/queries/technicalRecord/vehicleDetailsQuery.ts
+++ src/app/queries/technicalRecord/vehicleDetailsQuery.ts
@@ -1,13 +1,14 @@
 import { dateFormat } from '../../../constants';
 
-const baseQuery = 'SELECT v.id, JSON_OBJECT('
-  + "'system_number', v.system_number, "
-  + "'vin', v.vin, "
-  + "'vrm_trm', v.vrm_trm, "
-  + "'trailer_id',v.trailer_id, "
-  + `'createdAt', DATE_FORMAT(v.createdAt, '${dateFormat}') `
-  + ') result'
-  + ' FROM vehicle v';
+const baseQuery = `
+  SELECT v.id, JSON_OBJECT(
+    'system_number', v.system_number,
+    'vin', v.vin,
+    'vrm_trm', v.vrm_trm,
+    'trailer_id',v.trailer_id,
+    'createdAt', DATE_FORMAT(v.createdAt, '${dateFormat}')
+  ) result
+  FROM vehicle v`;
 
 const VEHICLE_DETAILS_VRM_QUERY = `${baseQuery} WHERE v.vrm_trm = ?`;
 const VEHICLE_DETAILS_VIN_QUERY = `${baseQuery} WHERE v.vin = ?`;
