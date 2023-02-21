f79e14b9041eb711c92abed00ad5a40a51c1e095
Guillaume Julien <guillaume.julien@bjss.com>
2023-02-21 12:44:08 +0000

    hopefully a fix

--- src/app/queries/evlQuery.ts
+++ src/app/queries/evlQuery.ts
@@ -1,11 +1,13 @@
 const EVL_QUERY = `
+SET @prev := '';
 SELECT testExpiryDate, vrm_trm, certificateNumber
 FROM (
-    SELECT testExpiryDate, vrm_trm, certificateNumber
+    SELECT testExpiryDate, vrm_trm, certificateNumber, IF(@prev <> vrm_trm, @rn:=0,@rn) AS row_number_over_partition, @prev:=vrm_trm, @rn:=@rn+1 AS rn
     FROM evl_view
 ) as SubQ
+WHERE row_number_over_partition = 0
 `;
 
-const EVL_VRM_QUERY = `${EVL_QUERY} WHERE vrm_trm = ?;`;
+const EVL_VRM_QUERY = `${EVL_QUERY} AND vrm_trm = ?;`;
 
 export { EVL_QUERY, EVL_VRM_QUERY };
