d2b7626f51c8c85dca880a420c538fe648519ac1
Guillaume Julien <guillaume.julien@bjss.com>
2023-02-21 11:59:25 +0000

    add dups

--- src/app/queries/evlQuery.ts
+++ src/app/queries/evlQuery.ts
@@ -1,12 +1,11 @@
 const EVL_QUERY = `
 SELECT testExpiryDate, vrm_trm, certificateNumber
 FROM (
-    SELECT testExpiryDate, vrm_trm, certificateNumber, IF(@prev <> vrm_trm, @rn:=0,@rn) AS row_number_over_partition, @prev:=vrm_trm, @rn:=@rn+1 AS rn
+    SELECT testExpiryDate, vrm_trm, certificateNumber
     FROM evl_view
 ) as SubQ
-WHERE row_number_over_partition = 0
 `;
 
-const EVL_VRM_QUERY = `${EVL_QUERY} AND vrm_trm = ?;`;
+const EVL_VRM_QUERY = `${EVL_QUERY} WHERE vrm_trm = ?;`;
 
 export { EVL_QUERY, EVL_VRM_QUERY };
