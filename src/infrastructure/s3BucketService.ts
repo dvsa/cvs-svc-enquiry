73fff1f877190b6689f6caaa4b6b27afdc608524
Guillaume Julien <guillaume.julien@bjss.com>
2023-05-24 17:12:17 +0100

    feat(cb2-8313): get latest file from S3

--- src/infrastructure/s3BucketService.ts
+++ src/infrastructure/s3BucketService.ts
@@ -14,6 +14,24 @@ export function uploadToS3(processedData: string, fileName: string, callback: ()
   });
 }
 
+export async function getLastAlphabeticalItemByPrefix(prefix: string): Promise<string> {
+  logger.info(`Getting latest file uploaded for prefix ${prefix}`);
+  const s3 = configureS3();
+  const params: AWS.S3.ListObjectsV2Request = { Bucket: process.env.AWS_S3_BUCKET_NAME, Prefix: prefix };
+  return getByPrefix(s3, params);
+}
+
+async function getByPrefix(s3: AWS.S3, params: AWS.S3.ListObjectsV2Request, allItems: string[] = []): Promise<string> {
+  const response = await s3.listObjectsV2(params).promise();
+  response.Contents.forEach((item) => allItems.push(item.Key));
+  if (response.NextContinuationToken) {
+    params.ContinuationToken = response.NextContinuationToken;
+    await getByPrefix(s3, params, allItems);
+  }
+  allItems.reverse();
+  return allItems[0];
+}
+
 function configureS3() {
   if (process.env.IS_OFFLINE === 'true') {
     logger.debug('configuring s3 using serverless');
