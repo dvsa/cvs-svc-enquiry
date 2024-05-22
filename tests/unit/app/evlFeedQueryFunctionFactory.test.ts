b9d4181f09c23c2477d039f03e7c52e1752ebb2c
Owen Corrigan (BJSS, VTM) <122352145+owen-corrigan-bjss@users.noreply.github.com>
2024-05-22 14:11:11 +0100

    chore(cb2-11273): dependency upgrades

--- tests/unit/app/evlFeedQueryFunctionFactory.test.ts
+++ tests/unit/app/evlFeedQueryFunctionFactory.test.ts
@@ -1,4 +1,3 @@
-import { mocked } from 'ts-jest/utils';
 import queryFunctionFactory from '../../../src/app/evlFeedQueryFunctionFactory';
 import * as dbFunctions from '../../../src/app/databaseService';
 import DatabaseService from '../../../src/infrastructure/databaseService';
@@ -7,8 +6,8 @@ import { FeedName } from '../../../src/interfaces/FeedTypes';
 jest.mock('../../../src/app/databaseService');
 jest.mock('../../../src/infrastructure/databaseService');
 
-const dbFunctionsMock = mocked(dbFunctions);
-const dbServiceMock = (mocked(DatabaseService, true) as unknown) as DatabaseService;
+const dbFunctionsMock = jest.mocked(dbFunctions);
+const dbServiceMock = (jest.mocked(DatabaseService) as unknown) as DatabaseService;
 
 describe('Query Function Factory', () => {
   it('returns the correct function when passed a vin', () => {
