d6e46f5814fe190b892dddd7f899063b658dce34
Leo <145344770+LGin-BJSS@users.noreply.github.com>
2024-05-23 11:24:51 +0100

    cb2-11778: upgrade mysql 2 (#84)
    * feat(cb2-11778): add explicit charset to mysql connection
    
    * feat(cb2-11778): additional lint change

--- src/infrastructure/databaseService.ts
+++ src/infrastructure/databaseService.ts
@@ -41,6 +41,7 @@ export default class DatabaseService implements DatabaseServiceInterface {
         port: dbConnectionDetails.port,
         database: process.env.SCHEMA_NAME,
         multipleStatements: true,
+        charset: 'utf8mb4',
       });
     }
 
