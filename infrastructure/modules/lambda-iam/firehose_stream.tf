747ea9251e3cadcfb3fd251086ddb45f93301bfd
Pete Mallam <pete.mallam@bjss.com>
2024-12-09 09:54:16 +0000

    Moar fixes and trimmin

--- infrastructure/modules/lambda-iam/firehose_stream.tf
+++ infrastructure/modules/lambda-iam/firehose_stream.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_subscription_filter" "filter" {
-  for_each        = var.enable_firehose ? toset(terraform.workspace) : []
+  for_each        = var.enable_firehose ? toset([terraform.workspace]) : []
   log_group_name  = aws_cloudwatch_log_group.logs.name
   name            = "${var.project}-${terraform.workspace}-${var.name}-${terraform.workspace}-metrics"
   destination_arn = data.aws_kinesis_firehose_delivery_stream.firehose_metrics[each.value].arn
