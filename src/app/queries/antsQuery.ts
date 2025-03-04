b6d010ac8a33fa160e14b89908f2b84eb9ad6ba2
Melanie Coslett <50204104+m-coslett@users.noreply.github.com>
2025-03-04 15:12:48 +0000

    feat(cb2-16540): formatting dates in query (#107)
    * feat(cb2-16540): formatting dates in query
    
    * feat(cb2-16540): corrected date separator

--- src/app/queries/antsQuery.ts
+++ src/app/queries/antsQuery.ts
@@ -4,11 +4,11 @@ SELECT
     make,
     model,
     wheelplan,
-    test_date,
+    DATE_FORMAT(test_date,'%d-%m-%Y') test_date,
     weight_before_test,
     weight_after_test,
     DOE_reference,
-    tech_record_date
+    DATE_FORMAT(tech_record_date,'%d-%m-%Y') tech_record_date
 FROM vw_dvla_ants
 WHERE test_date >= STR_TO_DATE(?, '%d/%m/%Y %T')
 ORDER BY test_date ASC;`;
